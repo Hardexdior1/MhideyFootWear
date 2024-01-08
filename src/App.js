7d8fee38aa39d3ca3eccb6f186ebe61c572b4528
adeniran <adeniranquwam001@gmail.com>
2024-01-08 11:39:24 +0100

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,95 +1,48 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LandingPage from "./Pages/LandingPage";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 import { useState } from "react";
-
-import NavBar from "./Pages/NavBar";
 import About from "./Pages/About";
+import Landing from "./Components/Landing";
+import Header from "./Components/Header";
+import Blog from "./Pages/Blog";
+import Contact from "./Pages/Contact";
+import Footer from "./Components/Footer";
+import Team from "./Pages/Team";
+import Career from "./Pages/Career";
 
-import products from "./Pages/Product";
 
-import Category from "./Pages/Category";
-import whatsAppIcon from "./Images/4494495.png";
-import Footer from "./Pages/Footer";
-
-const allItem = ["All", ...new Set(products.map((item) => item.category))];
-console.log(allItem);
 
 function App() {
-  const [product, setProduct] = useState(products);
 
-  const [itemsLength, setItemsLength] = useState("");
-  // const negotiable=products.filter((negotiableItem)=>{
-  //   if(negotiableItem.negotiable==true){
-  //     setProduct(negotiable)
-  //   }
-  // })
-  // console.log(negotiable);
-
-  const negotiableItem = () => {
-    const newItems2 = products.filter((items) => items.negotiable == true);
-    setProduct(newItems2);
-    setItemsLength(newItems2.length + " wears found for Negotiable wears ");
-  };
-
-  const notNegotiableItem = () => {
-    const newItems3 = products.filter((items) => items.negotiable == false);
-    setProduct(newItems3);
-    setItemsLength(newItems3.length + " wears found for Non-Negotiable wears ");
-  };
-
-  const filterItems = (category) => {
-    if (category == "All") {
-      setProduct(products);
-      setItemsLength("");
-
-      return;
-    }
-    const newItems = products.filter((item) => item.category == category);
-    setProduct(newItems);
-    setItemsLength(newItems.length + " wears found for " + category);
-  };
+ 
   return (
-    <div className="App">
-      {/* +234 702 639 9492 */}
-      {/* https://wa.me/message/BPARJ6I5JM6PL1 */}
-      {/* belloayomide36@gmail.com */}
-      {/* <button onClick={()=>{
-        negotiableItem()
-      }}>negotiable Goods </button>
-      
-      I'm Bello Ayomide, a Banking and Finance graduate from Laspotech. Despite my academic background, my passion led me to excel in the footwear business. With 2 years of dedicated experience in the footwear industry, I've not only developed a strong sense of customer relations but also cultivated a friendly and welcoming approach. My journey has equipped me with valuable insights into both the financial and footwear aspects of business, and I'm eager to continue making strides in this dynamic field.
+//     
 
-<button onClick={()=>{
-        notNegotiableItem()
-      }}>Fixed  price  </button> */}
+    <div className="App">
+   
       <BrowserRouter>
-        <NavBar />
+      <Header />
         <Routes>
-          {/* <Route path="/" element={<Category  />} />  */}
-          <Route
-            path="/"
-            element={
-              <LandingPage
-                product={product}
-                category={allItem}
-                filterItems={filterItems}
-                negotiableItem={negotiableItem}
-                notNegotiableItem={notNegotiableItem}
-                itemsLength={itemsLength}
-              />
-            }
-          />
+          <Route path="/" element={<Landing />} /> 
+          
+
           <Route path="/About" element={<About />} />
+          <Route path="/Blog" element={<Blog />} />
+          <Route path="/Contact" element={<Contact />} />
+          <Route path="/Team" element={<Team />} />
+          <Route path="/Career" element={<Career />} />
+
+
+
+
         </Routes>
 
         <Footer />
+
       </BrowserRouter>
-      <a href="https://wa.me/message/BPARJ6I5JM6PL1">
-        {" "}
-        <img className="icon" src={whatsAppIcon} alt="" />
-      </a>
+
+<a href="">REFRESH</a>
     </div>
   );
 }
--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,33 +1,166 @@
-import React from "react";
+import React, { useEffect } from "react";
+import "../Styles/Hero.css";
 import { Link } from "react-router-dom";
-import "../Styles/About.css";
-import mhidey from "../Images/mhidey.jpeg";
+import woman from "../images/woman.png";
+import family from "../images/family.png";
+import couples from "../images/couples.png";
+import blur from '../images/blur.png'
+import max from '../images/maxresdefault2.jpg'
+
 
 const About = () => {
+  useEffect(()=>{
+    window.scrollTo(0,0)
+  },[])
   return (
     <div>
-      <section className="aboutWrap1">
-        <div>
-          <h1>hello, I'm Bello Ayomide</h1>
+      <section className="heroSection">
+        <h1>How we got started </h1>
+        <p>
+          From a tiny garage to a multi-million dollar company, we innovated our
+          way to the top.{" "}
+        </p>
+        <Link>
+          <button>LEARN MORE </button>
+        </Link>
+      </section>
+
+      <section className="makingWrap">
+        <div className="makingSection">
+          <h4>MAKING THE WORLD A BETTER PLACE</h4>
+          <p>
+            Samsung a renowned South Korean company and one of the world’s
+            largest producers of electronic devices. The company specializes in
+            the production of a wide range of consumer and industrial
+            electronics, including digital media devices, appliances, memory
+            chips, semiconductors, and integrated systems. With its exceptional
+            technological advancements, Samsung has become a well-known name in
+            the industry and accounts for approximately one fifth of South
+            Korea’s total exports.
+          </p>
+        </div>
+
+        <div className="samsungCompanyWrap">
+          <div><img src={max} alt="video-img" /></div>
+          <div className="ourStoryFlex">
+            <div>
+              <h4 className="story">Our Story </h4>
+              <p>
+                Samsung Electronics constantly reinvents the future. We explore
+                the unknown to discover technologies to help people all over the
+                world lead happier, healthier lives.
+              </p>
+
+              <div className="flexJoin">
+                <button>JOIN OUR TEAM </button>
+              </div>
+            </div> 
+
+           {/* <div className="gridImages">
+              <div >
+                <img  className="woman" src={woman} alt="woman-img" />
+              </div>
+              <div>
+                <div>
+                  <img src={family} alt="family-img" />
+                </div>
+                <div>
+                  <img src={couples} alt="couples-img" />
+                </div>
+              </div>
+            </div> */}
+          </div>
+
+           <div>
+            <h1 className="centerH1">ABOUT SAMSUNG PROMOTION </h1>
+            <p>
+              Samsung Promotion and Award is a unique initiative launched by
+              Samsung and its partners to give back to the community. Every
+              month, Samsung conducts a Lucky Draw with the assistance of ten
+              reputable brands such as Google LLC, Oakland Bank, and Nestle. The
+              selection of winners is completely random, based on an AI system
+              that operates across various social media platforms including
+              Facebook and Instagram. Lucky beneficiaries stand a chance to
+              receive attractive rewards such as cash prizes, Samsung mobile
+              phones, TVs, and other exciting gifts. This program is also known
+              as Samsung Lottery, Samsung Competition Winners, Samsung Lottery
+              International, Samsung Promo Winners 2023, Samsung Lottery Awards
+              2023, Samsung Promotions, and Samsung Contest 2023. Once
+              announced, the winners are required to follow the process to claim
+              their awards.
+            </p>
+          </div> 
 
           <div>
-            <img src={mhidey} alt="Ayomide" />
+            <h1 className="centerH1">HOW SAMSUNG PROMOTION WORKS</h1>
+            <p>
+              Samsung Electronics is excited to announce the launch of its 2023
+              Lucky Beneficiaries program, which offers the opportunity for
+              selected participants to receive special rewards and benefits. To
+              be considered for this program, participants are required to sign
+              up on Samsung’s website and provide their basic personal
+              information. The selection process will be conducted using a
+              computerized programmable AI network, which will randomly select
+              the lucky beneficiaries from the pool of eligible participants.
+            </p>
+            <p>
+              To ensure your eligibility, please make sure to provide accurate
+              and up-to-date information during the sign-up process. Please note
+              that incomplete or incorrect information may result in
+              disqualification from the program. All selected participants will
+              be contacted by email or phone, so please ensure that you provide
+              valid contact information during the sign-up process.
+            </p>
+            <p>
+              We are thrilled to offer this exciting opportunity to our valued
+              customers, and we look forward to rewarding our lucky
+              beneficiaries with amazing prizes and benefits. Thank you for your
+              continued support of Samsung Electronics, and we wish you the best
+              of luck in the selection process.
+            </p>
+            <p>
+              To obtain a random BDC code for verification by the Samsung
+              promotion manager, the selected individual needs to follow a few
+              simple steps. First, please visit Samsung’s website 
+              and provide accurate and complete
+              information while working with the online publicist agent, Amelia
+              Bradman.
+            </p>
+
+            <p>
+              To contact Amelia, please click the following link:{" "}
+              <Link>email@example.com</Link> .
+            </p>
+
+            <p>
+              Once you have provided all necessary information and worked with
+              Amelia to verify your eligibility, you will be Directed on how to
+              generate an Automated BDC code that will be used to claim your
+              prize. We appreciate your cooperation and look forward to hearing
+              from you soon.
+            </p>
+          </div> 
+        </div>
+
+        
+
+      </section>
+       <div className="undefinedImgAndTextWrap">
+        <div>
+        
+          <p>With great A.I Power comes great responsibility, in the field of technology</p>
+
+          <div>
+            <i>Tony Stark,</i>
+            <h4>STARK INDUSTRIES</h4>
           </div>
         </div>
 
-       <div>
-       <p>
-         I am  a Banking and Finance student at Laspotech. Despite my academic
-          background, my passion led me to excel in the footwear business. With
-          2 years of dedicated experience in the footwear business, I've not
-          only developed a strong sense of customer relations but also
-          cultivated a friendly and welcoming approach. My journey has equipped
-          me with valuable insights into both the financial and footwear aspects
-          of business, and I'm eager to continue making strides in this dynamic
-          field.... click <Link to='/'>here</Link> to go back to the shop page 
-        </p>
-       </div>
-      </section>
+        <div>
+          <img src={ blur} alt="blur-img" />
+        </div>
+
+      </div> 
     </div>
   );
 };
